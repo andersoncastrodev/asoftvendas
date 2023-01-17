43d164561decff848a8337ca3daf9bf4391c5276
Anderson Castro <andersoncastrosantos@gmail.com>
2023-01-17 10:23:39 -0300

    Estado Controller

--- src/main/java/com/asoft/controller/EstadoController.java
+++ src/main/java/com/asoft/controller/EstadoController.java
@@ -1,30 +1,43 @@
 package com.asoft.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.asoft.model.Estado;
 import com.asoft.service.EstadoService;
 
 @RestController
-@RequestMapping("/estado")
+@RequestMapping("/estados")
 public class EstadoController {
 
 	@Autowired
 	private EstadoService estadoService;
-	
+		
 	@GetMapping
-	public void hello() {
-		System.out.println(">>> TESTE <<<<");
-	}
-	
-	@GetMapping("/estados")
 	public List<Estado> findEstados() {
 		
-	     return estadoService.listEstados();
+	     return estadoService.listaEstados();
 	}
+	
+	@GetMapping("/id")
+	public Optional<Estado> buscar(@PathVariable Long id) {
+		
+		return estadoService.buscaId(id);
+		
+	}
+	
+	@PostMapping
+	public Estado adicionar(@RequestBody Estado estado) {
+
+		return estadoService.salvar(estado);
+	}
+	
 }
--- src/main/java/com/asoft/service/EstadoService.java
+++ src/main/java/com/asoft/service/EstadoService.java
@@ -1,6 +1,7 @@
 package com.asoft.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,8 +15,16 @@ public class EstadoService {
 	@Autowired
 	private EstadoRepository estadoRepository;
 	
-	public List<Estado> listEstados(){
+	public List<Estado> listaEstados(){
 		List<Estado> lista = estadoRepository.findAll();
 		return lista;
+	}
+	
+	public Optional<Estado> buscaId(Long id) {	
+		return estadoRepository.findById(id);
+	}
+	
+	public Estado salvar(Estado estado) {
+		return estadoRepository.save(estado);
 	}
 }
