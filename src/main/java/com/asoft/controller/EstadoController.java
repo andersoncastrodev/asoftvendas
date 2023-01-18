29131bf995c8f751dffe35f5ee85e56a5f309b4d
Anderson Castro <andersoncastrosantos@gmail.com>
2023-01-18 18:21:30 -0300

    Adicionando ReponseEntity

--- src/main/java/com/asoft/controller/EstadoController.java
+++ src/main/java/com/asoft/controller/EstadoController.java
@@ -2,15 +2,20 @@ package com.asoft.controller;
 
 import java.util.List;
 import java.util.Optional;
-
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.asoft.exception.CodigoNaoExisteException;
 import com.asoft.model.Estado;
 import com.asoft.service.EstadoService;
 
@@ -22,22 +27,65 @@ public class EstadoController {
 	private EstadoService estadoService;
 		
 	@GetMapping
-	public List<Estado> findEstados() {
-		
-	     return estadoService.listaEstados();
+	public ResponseEntity<List<Estado>> listarTodos() {
+			
+	     return ResponseEntity.ok( estadoService.consultarTodos() ) ;
 	}
 	
 	@GetMapping("/id")
-	public Optional<Estado> buscar(@PathVariable Long id) {
+	public ResponseEntity<Estado> listarId(@PathVariable Long id) {
 		
-		return estadoService.buscaId(id);
+		Optional<Estado> estado = estadoService.consultarId(id);
 		
+		if (estado.isEmpty()) {
+			return ResponseEntity.badRequest().build();
+		}
+		
+		return ResponseEntity.ok(estado.get());	
 	}
 	
 	@PostMapping
-	public Estado adicionar(@RequestBody Estado estado) {
+	public ResponseEntity<Estado> adicionar(@RequestBody Estado estado) {
 
-		return estadoService.salvar(estado);
+		Estado estadoSalvo =  estadoService.salvar(estado);
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(estadoSalvo);
 	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Estado> atualizar(@PathVariable Long id,@RequestBody Estado estado) {
+		
+		Optional<Estado> estadoAtual = estadoService.consultarId(id);
+		
+		if(estadoAtual.isPresent() ) {
+			
+			BeanUtils.copyProperties(estado, estadoAtual.get(),"id");
+			
+			Estado estadoSalvo = estadoService.salvar(estadoAtual.get());
+			
+			return ResponseEntity.ok(estadoSalvo);	
+		}
+		
+		return ResponseEntity.notFound().build();
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> remover(@PathVariable Long id) {
+		
+		try {
+			
+			estadoService.excluir(id);
+			return ResponseEntity.noContent().build();
+			
+		} catch (CodigoNaoExisteException e) {
+			//return ResponseEntity.notFound().build();
+			return ResponseEntity.badRequest().body(e.getMessage()); //Se o codigo NÃO Existir
+		}
+			
+	}
+	
+	
+	
+	
 	
 }
--- src/main/java/com/asoft/service/EstadoService.java
+++ src/main/java/com/asoft/service/EstadoService.java
@@ -1,11 +1,13 @@
 package com.asoft.service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.asoft.exception.CodigoNaoExisteException;
 import com.asoft.model.Estado;
 import com.asoft.repository.EstadoRepository;
 
@@ -15,16 +17,33 @@ public class EstadoService {
 	@Autowired
 	private EstadoRepository estadoRepository;
 	
-	public List<Estado> listaEstados(){
+	public List<Estado> consultarTodos(){
 		List<Estado> lista = estadoRepository.findAll();
 		return lista;
 	}
 	
-	public Optional<Estado> buscaId(Long id) {	
+	public Optional<Estado> consultarId(Long id) {	
 		return estadoRepository.findById(id);
 	}
 	
 	public Estado salvar(Estado estado) {
+		
 		return estadoRepository.save(estado);
+	}
+	
+	public void excluir(Long estadoId) {
+		
+		try {
+
+			Optional<Estado> estado = estadoRepository.findById(estadoId);
+			
+			estadoRepository.deleteById( estado.get().getId() );
+			
+		} catch (NoSuchElementException e) {
+			
+			throw new CodigoNaoExisteException(String.format("Não existe um Estado com o codigo %d ", estadoId));
+		}
+
+		
 	}
 }
